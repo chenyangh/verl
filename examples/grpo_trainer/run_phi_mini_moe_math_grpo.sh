6b065acbef54ebdf476c3026a9708fcc0097fed8
Chenyang Huang <me@chenyangh.com>
2025-09-25 20:29:44 +0000

    add moe+verl

---
+++ examples/grpo_trainer/run_phi_mini_moe_math_grpo.sh
@@ -0,0 +1,52 @@
+set -x
+
+EXP_NAME="phi-moe-math-grpo"
+WANDB_API_KEY=496fa07a6ccb07d43292fe86646aff9c1a362b35
+WANDB_PROJECT=MoE-HW
+
+# MODEL_PATH=${MODEL_PATH:-/root/hf_cache/hub/models/microsoft/Phi-mini-MoE-instruct}
+MODEL_PATH="/root/hf_cache/hub/models/Qwen/Qwen2.5-3B-Instruct"
+
+
+TRAIN_PARQUET=${TRAIN_PARQUET:-/root/data/instruction_embedded_verl/train.parquet}
+VAL_PARQUET=${VAL_PARQUET:-/root/data/instruction_embedded_verl/val.parquet}
+
+python3 -m verl.trainer.main_ppo \
+    algorithm.adv_estimator=grpo \
+    data.train_files="$TRAIN_PARQUET" \
+    data.val_files="$VAL_PARQUET" \
+    data.train_batch_size=1024 \
+    data.max_prompt_length=1024 \
+    data.max_response_length=2048 \
+    data.filter_overlong_prompts=True \
+    data.truncation='error' \
+    data.prompt_key=prompt \
+    actor_rollout_ref.model.path=$MODEL_PATH \
+    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.actor.ppo_mini_batch_size=256 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=16 \
+    actor_rollout_ref.actor.use_kl_loss=True \
+    actor_rollout_ref.actor.kl_loss_coef=0.001 \
+    actor_rollout_ref.actor.kl_loss_type=low_var_kl \
+    actor_rollout_ref.actor.entropy_coeff=0 \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.actor.fsdp_config.param_offload=False \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=16 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
+    actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
+    actor_rollout_ref.rollout.n=5 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=16 \
+    actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    algorithm.use_kl_in_reward=False \
+    trainer.critic_warmup=0 \
+    trainer.logger='["console","wandb"]' \
+    trainer.project_name=$WANDB_PROJECT \
+    trainer.experiment_name=$EXP_NAME  \
+    trainer.n_gpus_per_node=8 \
+    trainer.nnodes=1 \
+    trainer.save_freq=20 \
+    trainer.test_freq=5 \
+    trainer.total_epochs=15 $@
